1f07cd2070c727fbf04ce17ba35684126a76e94f
Anderson Silva <odonto.andersonsilva@gmail.com>
2026-01-03 12:48:45 -0300

    ajustando a porta para o render

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ const config = new DocumentBuilder()
 
   app.enableCors();
 
-  await app.listen(4000);
+  const port = Number(process.env.PORT ?? 3000);
+  await app.listen(port,'0.0.0.0');
 }
 bootstrap();
